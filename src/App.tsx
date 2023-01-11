a698519621bd6f9b49d4856610d395281f3957af
EddyPBR <edvaldojunior1310@gmail.com>
2023-01-10 23:31:12 -0300

    :sparkles: Application integrated with static toDos list

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,75 @@
+import { useState } from "react";
+
+import { Container } from "./components/Container";
+import { CreateToDoForm } from "./components/CreateToDoForm";
+import { Header } from "./components/Header";
+import { ToDoList } from "./components/ToDoList";
+import { Resume } from "./components/Resume";
+
+interface IToDo {
+  id: string;
+  title: string;
+  isCompleted: boolean;
+}
+
+const toDosInitialState: IToDo[] = [
+  {
+    id: crypto.randomUUID(),
+    title:
+      "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
+    isCompleted: false,
+  },
+  {
+    id: crypto.randomUUID(),
+    title:
+      "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
+    isCompleted: false,
+  },
+  {
+    id: crypto.randomUUID(),
+    title:
+      "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
+    isCompleted: false,
+  },
+  {
+    id: crypto.randomUUID(),
+    title:
+      "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
+    isCompleted: true,
+  },
+  {
+    id: crypto.randomUUID(),
+    title:
+      "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
+    isCompleted: true,
+  },
+];
+
 export function App() {
-  return <div>ToDo List</div>;
+  const [toDos, setToDos] = useState<IToDo[]>(toDosInitialState);
+
+  const amountToDos = toDos.length;
+  const completedToDosAmount = toDos.filter((toDo) => toDo.isCompleted).length;
+
+  return (
+    <>
+      <Header />
+      <Container>
+        <CreateToDoForm
+          onCreateToDo={(newToDo) =>
+            setToDos((current) => [...current, newToDo])
+          }
+        />
+
+        <main style={{ width: "100%" }}>
+          <Resume
+            amount={amountToDos}
+            completedAmount={completedToDosAmount}
+            withDivider={amountToDos === 0}
+          />
+          <ToDoList toDos={toDos} onChangeToDo={setToDos} />
+        </main>
+      </Container>
+    </>
+  );
 }
