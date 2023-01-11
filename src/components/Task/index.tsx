8d25e2400691d5be48cc5225a81790ab7d3d0167
EddyPBR <edvaldojunior1310@gmail.com>
2023-01-10 22:29:19 -0300

    :sparkles: Task component now can be checked, deleted and styles adjusts

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,15 +1,25 @@
 import { Trash } from "phosphor-react";
+import { ChangeEvent } from "react";
 import styles from "./styles.module.css";
 
-export function Task() {
+interface ITaskProps {
+  title: string;
+  isChecked: boolean;
+  onCheck: (value: boolean) => void;
+  onDelete: () => void;
+}
+
+export function Task({ title, isChecked, onCheck, onDelete }: ITaskProps) {
+  function onChange(event: ChangeEvent<HTMLInputElement>) {
+    const value = event.target.checked;
+    onCheck(value);
+  }
+
   return (
     <div className={styles.task}>
-      <input type="radio" />
-      <p>
-        Integer urna interdum massa libero auctor neque turpis turpis semper.
-        Duis vel sed fames integer.
-      </p>
-      <button type="button">
+      <input type="checkbox" checked={isChecked} onChange={onChange} />
+      {isChecked ? <del>{title}</del> : <p>{title}</p>}
+      <button type="button" onClick={onDelete}>
         <Trash size={20} />
       </button>
     </div>
