07954ed40aca20733497599621654ed5127d6e84
EddyPBR <edvaldojunior1310@gmail.com>
2023-01-11 22:50:49 -0300

    :sparkles: start mirage server in development mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+import { startMirageServer } from "./__mock__";
+
 import "./global.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
@@ -8,3 +10,5 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <App />
   </React.StrictMode>
 );
+
+if (import.meta.env.MODE === "development") startMirageServer();
