5e41f3f6d01461c044f0c95340b8b87b4d5470fb
EddyPBR <edvaldojunior1310@gmail.com>
2023-01-12 22:37:22 -0300

    :sparkles: implements new api request to create ToDo

--- src/api/toDo.ts
+++ src/api/toDo.ts
@@ -10,9 +10,27 @@ interface IGetToDosResponse {
   toDos: IToDoDTO[];
 }
 
+interface ICreateToDoBody {
+  title: string;
+}
+
+interface ICreateToDoResponse {
+  toDo: IToDoDTO;
+}
+
 export async function getTodos() {
   try {
     return await api.get<IGetToDosResponse>("/todo");
+  } catch (error: any) {
+    throw apiException(error as Error);
+  }
+}
+
+export async function createToDo({ title }: ICreateToDoBody) {
+  try {
+    return await api.post<ICreateToDoResponse>("/todo", {
+      title,
+    });
   } catch (error: any) {
     throw apiException(error as Error);
   }
