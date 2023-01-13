d26d6f4f6424fc0623653444abe32c70ee3b31bb
EddyPBR <edvaldojunior1310@gmail.com>
2023-01-12 23:31:06 -0300

    :sparkles: implements new mock route to delete ToDo

--- src/__mock__/routes/toDoRoutes.ts
+++ src/__mock__/routes/toDoRoutes.ts
@@ -5,6 +5,11 @@ interface ICreateToDoBody {
   title: string;
 }
 
+interface IUpdateToDoBody {
+  title?: string;
+  isCompleted?: boolean;
+}
+
 export function toDoRoutes(server: Server) {
   server.get(`/todo`, (schema: AppSchema) => {
     const toDos = schema.all("toDo");
@@ -16,7 +21,7 @@ export function toDoRoutes(server: Server) {
     const { title } = JSON.parse(requestBody) as ICreateToDoBody;
 
     if (!title) {
-      return new Response(400, {}, { message: "Faltando o título da tarefa!" });
+      return new Response(400, {}, { message: "Faltando o título da tarefa" });
     }
 
     const newToDo = schema.create("toDo", {
@@ -32,17 +37,50 @@ export function toDoRoutes(server: Server) {
     const { id } = params;
 
     if (!id) {
-      return new Response(400, {}, { message: "Faltando ID da tarefa!" });
+      return new Response(400, {}, { message: "Faltando ID da tarefa" });
     }
 
-    const toDoExists = schema.find("toDo", id);
+    const toDo = schema.find("toDo", id);
 
-    if (!toDoExists) {
+    if (!toDo) {
+      return new Response(404, {}, { message: "Tarefa não foi encontrada" });
+    }
+
+    toDo.destroy();
+
+    return new Response(202);
+  });
+
+  server.patch(`/todo/:id`, (schema: AppSchema, { params, requestBody }) => {
+    const id = params.id;
+
+    if (!id) {
+      return new Response(400, {}, { message: "Faltando ID da tarefa" });
+    }
+
+    const { isCompleted, title } = JSON.parse(requestBody) as IUpdateToDoBody;
+
+    if (typeof(isCompleted) !== "boolean" && !title) {
+      return new Response(400, {}, { message: "Faltando corpo da requisição" });
+    }
+
+    const toDo = schema.find("toDo", id);
+
+    if (!toDo) {
       return new Response(404, {}, { message: "Tarefa não foi encontrada!" });
     }
 
-    toDoExists.destroy();
+    toDo.update({
+      isCompleted: isCompleted || toDo.isCompleted,
+      title: title || toDo.title,
+    });
 
-    return new Response(202);
+    return new Response(
+      202,
+      {},
+      {
+        toDo: toDo,
+      }
+    );
   });
 }
