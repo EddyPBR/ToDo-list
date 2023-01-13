d2d2302f8cafd7a0829e4ccff010f59294076733
EddyPBR <edvaldojunior1310@gmail.com>
2023-01-12 22:37:04 -0300

    :sparkles: add mock route to create ToDo

--- src/__mock__/routes/toDoRoutes.ts
+++ src/__mock__/routes/toDoRoutes.ts
@@ -1,10 +1,30 @@
 import { Response, Server } from "miragejs";
 import { AppSchema } from "../types";
 
+interface ICreateToDoBody {
+  title: string;
+}
+
 export function toDoRoutes(server: Server) {
   server.get(`/todo`, (schema: AppSchema) => {
     const toDos = schema.all("toDo");
 
     return new Response(200, {}, toDos);
+  });
+
+  server.post(`/todo`, (schema: AppSchema, { requestBody }) => {
+    const { title } = JSON.parse(requestBody) as ICreateToDoBody;
+
+    if (!title) {
+      return new Response(400, {}, { message: "Title is required!" });
+    }
+
+    const newToDo = schema.create("toDo", {
+      id: crypto.randomUUID(),
+      title,
+      isCompleted: false,
+    });
+
+    return new Response(202, {}, { toDo: newToDo });
   });
 }
